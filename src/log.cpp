b725e266cc16382e0150c7bc6bd90ccb9a6bcf43
xtarzx <telmomartins2000@gmail.com>
2023-09-07 12:53:46 +0100

    fix 'may be truncated' error

--- src/log.cpp
+++ src/log.cpp
@@ -36,68 +36,68 @@ using std::string;
 
 namespace L
 {
-    std::map<LEVEL, string> m_lmap{
-        {f, "FATAL"}, {e, "ERROR"}, {w, "WARN"}, {i, "INFO"}, {d, "DEBUG"}};
-    LEVEL g_level = L::i;
+        std::map<LEVEL, string> m_lmap{
+            {f, "FATAL"}, {e, "ERROR"}, {w, "WARN"}, {i, "INFO"}, {d, "DEBUG"}};
+        LEVEL g_level = L::i;
 
-    void setLogLevel(LEVEL level)
-    {
-        g_level = level;
-    }
+        void setLogLevel(LEVEL level)
+        {
+                g_level = level;
+        }
 
-    string getCurrTime()
-    {
-        struct timeval curTime;
-        char tStamp[50] = {}, ms[5] = {};
-        std::stringstream sstime;
+        string getCurrTime()
+        {
+                struct timeval curTime;
+                char tStamp[50] = {}, ms[5] = {};
+                std::stringstream sstime;
 
-        gettimeofday(&curTime, NULL);
+                gettimeofday(&curTime, NULL);
 #ifdef _WIN32
-        strftime(tStamp, sizeof(tStamp), "%Y-%m-%d %H:%M:%S", localtime((time_t *)&curTime.tv_sec));
+                strftime(tStamp, sizeof(tStamp), "%Y-%m-%d %H:%M:%S", localtime((time_t *)&curTime.tv_sec));
 #else
-        strftime(tStamp, sizeof(tStamp), "%F %T", localtime(&curTime.tv_sec));
+                strftime(tStamp, sizeof(tStamp), "%F %T", localtime(&curTime.tv_sec));
 #endif
-        snprintf(ms, 5, ".%03d", (int)curTime.tv_usec / 1000);
-        sstime << tStamp << ms;
-        return sstime.str();
-    }
+                snprintf(ms, 5, ".%03u", (int)curTime.tv_usec / 1000);
+                sstime << tStamp << ms;
+                return sstime.str();
+        }
 
-    string getLogPrefix(LEVEL level)
-    {
-        string lvl = m_lmap.at(level);
-        string time = getCurrTime();
-        std::ostringstream ostrout;
+        string getLogPrefix(LEVEL level)
+        {
+                string lvl = m_lmap.at(level);
+                string time = getCurrTime();
+                std::ostringstream ostrout;
 
 #ifdef __GLIBC__
-        pid_t t_id = syscall(__NR_gettid);
-        ostrout << time << " " << program_invocation_short_name;
+                pid_t t_id = syscall(__NR_gettid);
+                ostrout << time << " " << program_invocation_short_name;
 #else
-        pid_t t_id = getpid();
+                pid_t t_id = getpid();
 #ifdef _WIN32
-        LPSTR path{};
-        GetModuleFileName(0, path, MAX_PATH);
-        ostrout << time << " " << path;
+                LPSTR path{};
+                GetModuleFileName(0, path, MAX_PATH);
+                ostrout << time << " " << path;
 #else
-        ostrout << time << " " << getprogname();
+                ostrout << time << " " << getprogname();
 #endif
 #endif
-        ostrout << "(" << std::to_string(t_id) << ") ";
-        ostrout << lvl.c_str() << "\t: ";
-        return ostrout.str();
-    }
+                ostrout << "(" << std::to_string(t_id) << ") ";
+                ostrout << lvl.c_str() << "\t: ";
+                return ostrout.str();
+        }
 
-    void log(LEVEL lvl, const char *format, ...)
-    {
-        if (lvl > g_level)
-            return;
+        void log(LEVEL lvl, const char *format, ...)
+        {
+                if (lvl > g_level)
+                        return;
 
-        string mesg = getLogPrefix(lvl);
-        va_list argptr;
+                string mesg = getLogPrefix(lvl);
+                va_list argptr;
 
-        va_start(argptr, format);
-        fprintf(stdout, "%s", mesg.c_str());
-        vfprintf(stdout, format, argptr);
-        fprintf(stdout, "\n");
-        va_end(argptr);
-    }
+                va_start(argptr, format);
+                fprintf(stdout, "%s", mesg.c_str());
+                vfprintf(stdout, format, argptr);
+                fprintf(stdout, "\n");
+                va_end(argptr);
+        }
 }
